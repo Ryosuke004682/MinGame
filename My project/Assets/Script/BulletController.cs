d1ee2d87e3ae8363207ca9d0e66209edc0e3073f
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-03-13 22:51:58 +0900

    敵を消せるようにした

--- My project/Assets/Script/BulletController.cs
+++ My project/Assets/Script/BulletController.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-
     public void Shot(Vector3 direction)
     {
         GetComponent<Rigidbody>().AddForce(direction);
     }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Target"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
 }
+
